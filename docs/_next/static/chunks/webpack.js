cca9d42cbf9d0b6fd73c3f79a609478f7ebc79dc
Tyler Buchea <tyler@buchea.com>
2022-04-18 11:45:04 -0400

    New build

--- docs/_next/static/chunks/webpack.js
+++ docs/_next/static/chunks/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0244a41bf90f4079525d";
+/******/ 	var hotCurrentHash = "d252f0af6aacf6925801";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
