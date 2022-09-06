e2b95b1a90662a13aca0aa77cf7490d807a18ad3
Tyler Buchea <tyler@buchea.com>
2022-09-06 10:39:23 -0400

    Update companies and pfp

--- components/Card.js
+++ components/Card.js
@@ -6,89 +6,82 @@ export default function Card({ title, sub, img, color, href }) {
   const hrefAmp = isAmp ? `${href}?amp=1` : `${href}`;
 
   return (
-    <div className="spacer">
-      <div className="card" style={{ background: color }}>
-        <div className="image">
-          <amp-img alt="story" layout="fill" src={img}></amp-img>
-        </div>
-        <a href={hrefAmp} target="_blank" rel="noreferrer">
-          <h5>
-            {title} <i className="fa fa-link" />
-          </h5>
-        </a>
-        <p>{sub}</p>
-
-        <style jsx global>{`
-          amp-img img {
-            object-fit: contain;
-            margin: 0px auto;
-          }
-          // .i-amphtml-loader-background {
-          //   background: rgba(0, 0, 0, 0.3);
-          // }
-          // .i-amphtml-new-loader {
-          //   color: #fff;
-          // }
-        `}</style>
-        <style jsx>{`
-          .spacer {
-            width: 300px;
-            height: 100%;
-            display: flex;
-            align-items: center;
-            justify-content: center;
-          }
-
-          .card {
-            padding: 2vw;
-            height: calc(100% - 10px);
-            width: calc(100% - 10px);
-            justify-content: flex-end;
-            flex-direction: column;
-            display: flex;
-            border-radius: 2vw;
-          }
-
-          .image {
-            position: relative;
-            width: 100%;
-            height: 100%;
-            text-align: center;
-          }
-
-          h5 {
-            font-size: 1.2rem;
-            font-weight: bold;
-            color: #fff;
-            padding-top: 1vh;
-            padding-bottom: 0;
-            display: inline-block;
-            transition: border-bottom 0.3s ease-in-out;
-            font-family: "Roboto", sans-serif;
-            border-bottom: 2px solid transparent;
-          }
-
-          a:hover h5 {
-            border-bottom: 2px solid #fff;
-          }
-
-          a:active h5 {
-            border-bottom: 2px solid #fff;
-            transition: none;
-          }
-
-          p {
-            margin: 0;
-            padding-top: 2vh;
-            padding-bottom: 3vh;
-            color: #fff;
-            z-index: 1;
-            font-size: 1rem;
-            height: 90px;
-            font-family: "Roboto", sans-serif;
-          }
-        `}</style>
+    <div className="card" style={{ background: color }}>
+      <div className="image">
+        <amp-img alt="story" layout="fill" src={img}></amp-img>
       </div>
+      <a href={hrefAmp} target="_blank" rel="noreferrer">
+        <h5>
+          {title} <i className="fa fa-link" />
+        </h5>
+      </a>
+      <p>{sub}</p>
+
+      <style jsx global>{`
+        amp-img img {
+          object-fit: contain;
+          margin: 0px auto;
+        }
+        // .i-amphtml-loader-background {
+        //   background: rgba(0, 0, 0, 0.3);
+        // }
+        // .i-amphtml-new-loader {
+        //   color: #fff;
+        // }
+      `}</style>
+      <style jsx>{`
+        .card {
+          justify-content: flex-end;
+          flex-direction: column;
+          display: flex;
+          border-radius: 50px;
+          padding: 20px;
+          border: 2px solid black;
+        }
+
+        .image {
+          position: relative;
+          width: 100%;
+          height: 100%;
+          text-align: center;
+          flex: 1 1 auto;
+          width: 100%;
+          height: 200px;
+          display: flex;
+          justify-content: center;
+        }
+
+        h5 {
+          font-size: 1.2rem;
+          font-weight: bold;
+          color: #fff;
+          padding: 15px 0px 0px 0px;
+          display: inline-block;
+          transition: border-bottom 0.3s ease-in-out;
+          font-family: "Roboto", sans-serif;
+          border-bottom: 2px solid transparent;
+        }
+
+        a:hover h5 {
+          border-bottom: 2px solid #fff;
+        }
+
+        a:active h5 {
+          border-bottom: 2px solid #fff;
+          transition: none;
+        }
+
+        p {
+          margin: 0;
+          padding: 20px 0px 80px 0px;
+          /* background: rgba(255, 0, 0, 0.5); */
+          color: #fff;
+          z-index: 1;
+          font-size: 1rem;
+          height: 90px;
+          font-family: "Roboto", sans-serif;
+        }
+      `}</style>
     </div>
   );
 }
--- components/Portfolio.js
+++ components/Portfolio.js
@@ -7,6 +7,14 @@ export default function Portfolio() {
       <h4>Companies</h4>
       <Slider>
         <Card
+          key="Dexloan"
+          title="Dexloan"
+          sub="Unlock the value of your NFTs with escrowless borrowing & lending."
+          img="dexloan3.png"
+          color="linear-gradient(162deg, #2f855a 19%, #2d3748 97%)"
+          href="https://twitter.com/dexloanio"
+        />
+        <Card
           key="Chicken Tribe"
           title="Chicken Tribe"
           sub="Building no-code launchpad services for holders. Engaging the community through comic page-style bits of lore."
@@ -15,20 +23,20 @@ export default function Portfolio() {
           href="https://twitter.com/ChickenTribe"
         />
         <Card
+          key="Exiled Apes"
+          title="Exiled Apes"
+          sub="A historic collection of only 518 NFTs. First mint of the Degen Ape Academy. Solana's only misprint NFT."
+          img="exiled-logo.png"
+          color="linear-gradient(162deg, #4e4e50 19%, #1c1c1c 97%)"
+          href="https://twitter.com/ExiledApes"
+        />
+        <Card
           key="Solana Sanctuary"
           title="Solana Sanctuary"
           sub="The Sanctuary is a DAO dedicated to providing resources to Solana NFT communities that have been rugged."
           img="sanctuary.png"
           color="linear-gradient(162deg, #61f9c7 19%, #7499ff 97%)"
           href="https://twitter.com/SolanaSanctuary"
-        />
-        <Card
-          key="Exiled Apes"
-          title="Exiled Apes"
-          sub="A historic collection of only 518 NFTs. First mint of the Degen Ape Academy. Solana's only misprint NFT."
-          img="exiled-logo.png"
-          color="linear-gradient(162deg, rgb(51, 51, 51) 19%, rgb(40, 40, 40) 97%)"
-          href="https://twitter.com/ExiledApes"
         />
       </Slider>
       <style jsx>{`
