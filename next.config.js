765b970511cbb243cc87bb74f0f4f6501e9d569d
Tyler Buchea <tyler@buchea.com>
2023-04-11 13:04:06 -0400

    Fix canvas

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
+// For building on vercel: https://github.com/Automattic/node-canvas/issues/1779
+if (
+  process.env.LD_LIBRARY_PATH == null ||
+  !process.env.LD_LIBRARY_PATH.includes(
+    `${process.env.PWD}/node_modules/canvas/build/Release:`
+  )
+) {
+  process.env.LD_LIBRARY_PATH = `${
+    process.env.PWD
+  }/node_modules/canvas/build/Release:${process.env.LD_LIBRARY_PATH || ""}`;
 }
 
-module.exports = nextConfig
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
